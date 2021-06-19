ba960b0d6fa6619199353b0bfec4dc86e25a1814
Rakesh Sharma <Rakesh Sharma@LAPTOP-AKSMRTPR>
2021-06-19 19:08:35 +0530

    this is jenkin related

---
+++ Autodeskjenkins'/src/test/java/demoname/AutoDesksample.java
@@ -0,0 +1,12 @@
+package demoname;
+
+public class AutoDesksample {
+    public static void main(String[] args) {
+		System.out.println("hi");
+		String browser = System.getProperty("BROWSER");
+		String url = System.getProperty("URL");
+		System.out.println(browser);
+		System.out.println(url);
+
+	}
+}
